c53077604aa333e252624652cb99f991f5cd2bb8
chesarin <vargasces@gmail.com>
2012-10-22 08:10:41 -0400

    last changes

--- ftp-server/server.py
+++ ftp-server/server.py
@@ -4,26 +4,34 @@ import time
 from pyftpdlib import ftpserver
 
 now = lambda: time.strftime("[%Y-%b-%d %H:%M:%S]")
-f1 = open('ftpd.log', 'a')
-f2 = open('ftpd.lines.log', 'a')
+flog = open('ftpd.log', 'a')
+clog = open('commands-ftpd.log', 'a')
+elog = open('errors-ftpd.log','a')
+software_name = 'pure-ftp'
+software_version = '1.0.22'
 
-def standard_logger(msg):
-    f1.write("%s %s\n" %(now(), msg))
-    f1.flush()
+def ftpd_logger(msg):
+    flog.write("%s %s\n" %(now(), msg))
+    flog.flush()
 
-def line_logger(msg):
-    f2.write("%s %s\n" %(now(), msg))
-    f2.flush()
+def command_logger(msg):
+    clog.write("%s %s\n" %(now(), msg))
+    clog.flush()
 
+def error_logger(msg):
+    elog.write("%s %s\n" %(now(), msg))
+    elog.flush()
+    
 def main():
-    ftpserver.log = standard_logger
-    ftpserver.logline = line_logger
+    ftpserver.log = ftpd_logger
+    ftpserver.logline = command_logger
+    ftpserver.logerror = error_logger
     # Instantiate a dummy authorizer for managing 'virtual' users
     authorizer = ftpserver.DummyAuthorizer()
 
     # Define a new user having full r/w permissions and a read-only
     # anonymous user
-    authorizer.add_user('user', password="12345", homedir='.', perm='elradfmw')
+    authorizer.add_user('test', password="12345", homedir='.', perm='elradfmw')
     authorizer.add_anonymous(homedir='.')
 
     # Instantiate FTP handler class
@@ -31,7 +39,7 @@ def main():
     handler.authorizer = authorizer
 
     # Define a customized banner (string returned when client connects)
-    handler.banner = "pyftpdlib %s based ftpd ready." %ftpserver.__ver__
+    handler.banner = "%s %s ready." %(software_name, software_version)
 
     # Specify a masquerade address and the range of ports to use for
     # passive connections.  Decomment in case you're behind a NAT.
